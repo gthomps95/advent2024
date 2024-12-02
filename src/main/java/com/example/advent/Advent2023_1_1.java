48fd710a197cf9f541c7383c2894acdc3b938bab
Greg Thompson <gregorythompson@Gregorys-MacBook-Pro.local>
2024-12-01 20:04:38 -0600

    2024 day 1 parts 1 and 2

--- src/main/java/com/example/advent/Advent2023_1_1.java
+++ src/main/java/com/example/advent/Advent2023_1_1.java
@@ -1,89 +1,89 @@
-// package com.example.advent;
+package com.example.advent;
 
-// import java.io.FileInputStream;
-// import java.nio.charset.Charset;
+import java.io.FileInputStream;
+import java.nio.charset.Charset;
 
-// import org.apache.commons.io.IOUtils;
-// import org.springframework.boot.CommandLineRunner;
-// import org.springframework.stereotype.Component;
+import org.apache.commons.io.IOUtils;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
 
-// import lombok.extern.slf4j.Slf4j;
-// import lombok.val;
+import lombok.extern.slf4j.Slf4j;
+import lombok.val;
 
-// @Slf4j
+@Slf4j
 // @Component
-// public class Advent2023_1_1 implements CommandLineRunner {
+public class Advent2023_1_1 implements CommandLineRunner {
 
-// 	@Override
-// 	public void run(String... args) throws Exception {
-// 		val is = new FileInputStream("/Users/gregorythompson/Projects/advent/2023_1.txt");
+	@Override
+	public void run(String... args) throws Exception {
+		val is = new FileInputStream("/Users/gregorythompson/Projects/advent/2023_1.txt");
 
-// 		val lines = IOUtils.readLines(is, Charset.defaultCharset());
+		val lines = IOUtils.readLines(is, Charset.defaultCharset());
 
-// 		val sum = lines.stream().mapToLong(l -> {
-// 			val rf = replaceWordsForward(l);
-// 			val rb = replaceWordsBackword(l);
-// 			val first = getFirst(rf);
-// 			val last = getLast(rb);
+		val sum = lines.stream().mapToLong(l -> {
+			val rf = replaceWordsForward(l);
+			val rb = replaceWordsBackword(l);
+			val first = getFirst(rf);
+			val last = getLast(rb);
 
-// 			// log.info("f {}", first);
-// 			// log.info("l {}", last);
+			// log.info("f {}", first);
+			// log.info("l {}", last);
 
-// 			int value = (10 * first) + last;
+			int value = (10 * first) + last;
 
-// 			log.info("v {} {}", value, l);
+			log.info("v {} {}", value, l);
 
-// 			return value;
-// 		}).sum();
-// 		log.info("Answer is {}", sum);
-// 	}
+			return value;
+		}).sum();
+		log.info("Answer is {}", sum);
+	}
 		
-// 	private int getFirst(String l) {
-// 		return l.chars().filter(c -> isNumber(c)).findFirst().orElse(0) - '0';
-// 	}
+	private int getFirst(String l) {
+		return l.chars().filter(c -> isNumber(c)).findFirst().orElse(0) - '0';
+	}
 
-// 	private int getLast(String l) {
-// 		return new StringBuilder(l).reverse().toString().chars().filter(c -> isNumber(c)).findFirst().orElse(0) - '0';
-// 	}
+	private int getLast(String l) {
+		return new StringBuilder(l).reverse().toString().chars().filter(c -> isNumber(c)).findFirst().orElse(0) - '0';
+	}
 
-// 	boolean isNumber(int c) {
-// 		return c <= '9' && c >= '0';
-// 	}
+	boolean isNumber(int c) {
+		return c <= '9' && c >= '0';
+	}
 
-// 	private String replaceWordsBackword(String l) {
-// 		return l
-// 		.replace("zerone", "1")
-// 		.replace("zero", "0")
-// 		.replace("oneight", "8")
-// 		.replace("one", "1")
-// 		.replace("two", "2")
-// 		.replace("threight", "8")
-// 		.replace("three", "3")
-// 		.replace("four", "4")
-// 		.replace("fiveight", "8")
-// 		.replace("five", "5")
-// 		.replace("six", "6")
-// 		.replace("seven", "7")
-// 		.replace("eight", "8")
-// 		.replace("nine", "9")
-// 		;
-// 	}
+	private String replaceWordsBackword(String l) {
+		return l
+		.replace("zerone", "1")
+		.replace("zero", "0")
+		.replace("oneight", "8")
+		.replace("one", "1")
+		.replace("two", "2")
+		.replace("threight", "8")
+		.replace("three", "3")
+		.replace("four", "4")
+		.replace("fiveight", "8")
+		.replace("five", "5")
+		.replace("six", "6")
+		.replace("seven", "7")
+		.replace("eight", "8")
+		.replace("nine", "9")
+		;
+	}
 
-// 	private String replaceWordsForward(String l) {
-// 		return l
-// 		.replace("zero", "0")
-// 		.replace("twone", "2")
-// 		.replace("one", "1")
-// 		.replace("eightwo", "8")
-// 		.replace("eighthree", "8")
-// 		.replace("two", "2")
-// 		.replace("three", "3")
-// 		.replace("four", "4")
-// 		.replace("five", "5")
-// 		.replace("six", "6")
-// 		.replace("seven", "7")
-// 		.replace("eight", "8")
-// 		.replace("nine", "9")
-// 		;
-// 	}
-// }
+	private String replaceWordsForward(String l) {
+		return l
+		.replace("zero", "0")
+		.replace("twone", "2")
+		.replace("one", "1")
+		.replace("eightwo", "8")
+		.replace("eighthree", "8")
+		.replace("two", "2")
+		.replace("three", "3")
+		.replace("four", "4")
+		.replace("five", "5")
+		.replace("six", "6")
+		.replace("seven", "7")
+		.replace("eight", "8")
+		.replace("nine", "9")
+		;
+	}
+}
--- src/main/java/com/example/advent/Advent2024_1_1.java
+++ src/main/java/com/example/advent/Advent2024_1_1.java
@@ -1,9 +1,15 @@
 package com.example.advent;
 
+import java.io.FileInputStream;
+import java.nio.charset.Charset;
+import java.util.stream.IntStream;
+
+import org.apache.commons.io.IOUtils;
 import org.jline.utils.Log;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
+import lombok.val;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
@@ -13,6 +19,41 @@ public class Advent2024_1_1 implements CommandLineRunner{
     @Override
     public void run(String... args) throws Exception {
         log.info("2024 1 1");
+
+        val is = new FileInputStream("/Users/gregorythompson/Projects/advent/2024_1_1.txt");
+
+		val lines = IOUtils.readLines(is, Charset.defaultCharset());
+
+        val lineInts = lines.stream().map(l -> convertToInts(l)).toList();
+        val list1 = lineInts.stream().mapToInt(li -> li.a).sorted().toArray();
+        val list2 = lineInts.stream().mapToInt(li -> li.b).sorted().toArray();
+
+        int size = list1.length;
+        val sum = IntStream.range(0, size).map(i -> Math.abs(list2[i] - list1[i])).sum();
+
+
+        log.info("part 1 {}", sum); 
+
+        val counts = IntStream.range(0, size).mapToObj(i -> new Ints(list1[i], (int) findCount(list1[i], list2))).toList();
+
+        val sum2 = counts.stream().mapToLong(i -> i.a * i.b).sum();
+
+        counts.stream().forEach(i -> log.info("{} {}", i.a, i.b));
+        
+        log.info("part 2 {}", sum2);
+        
+        
+    } 
+
+    private Ints convertToInts(String l) {
+        val split = l.split("   ");
+        return new Ints(Integer.parseInt(split[0]), Integer.parseInt(split[1]));
     }
+
+    private long findCount(int s, int[] list){
+        return IntStream.range(0, list.length).filter(i -> list[i] == s).count();
+    }
+
+    private record Ints(int a, int b) {};
 
 }
