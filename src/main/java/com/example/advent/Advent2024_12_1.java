16266e68a349df7ceadfaaa9218a9659f42d0f56
Greg Thompson <gthomps95@gmail.com>
2024-12-12 22:32:54 -0600

    Day 12 part 1 and 2

--- src/main/java/com/example/advent/Advent2024_12_1.java
+++ src/main/java/com/example/advent/Advent2024_12_1.java
@@ -27,6 +27,13 @@ public class Advent2024_12_1 implements CommandLineRunner {
         p -> new Point(p.x - 1, p.y)
     );
 
+    private static List<Function<Point, Point[]>> MOVE_NEXT_C = List.of(
+        p -> new Point[] {new Point(p.x, p.y - 1), new Point(p.x + 1, p.y - 1), new Point(p.x + 1, p.y)},
+        p -> new Point[] {new Point(p.x + 1, p.y), new Point(p.x + 1, p.y + 1), new Point(p.x, p.y + 1)},
+        p -> new Point[] {new Point(p.x, p.y + 1), new Point(p.x - 1, p.y + 1), new Point(p.x - 1, p.y)},
+        p -> new Point[] {new Point(p.x - 1, p.y), new Point(p.x - 1, p.y - 1), new Point(p.x, p.y - 1)}
+    );
+
     @Override
     public void run(String... args) throws Exception {
         log.info("2024 12 1");
@@ -36,21 +43,27 @@ public class Advent2024_12_1 implements CommandLineRunner {
         val lines = IOUtils.readLines(is, Charset.defaultCharset());
 
         val map = create2dArray(lines);
-        printArray(map);
+        // printArray(map);
 
         val regions = findRegions(map);
-        log.info("regions {}", regions);
+        // log.info("regions {}", regions);
 
         val perims = findPerimeters(map, regions);
+        val corners = findCorners(map, regions);
 
         val cost = perims.stream().mapToInt(p -> p.length * p.area.points.size()).sum();
+        val cost2 = corners.stream().mapToInt(c -> c.count * c.area.points.size()).sum();
 
 
         log.info("day 12 part 1 {}", cost); 
-        // log.info("day 10 part 2 {}", ratings); 
+        log.info("day 12 part 2 {}", cost2); 
     } 
 
     private record Perimeter(int length, Region area) {
+
+    }
+
+    private record Corner(int count, Region area) {
 
     }
 
@@ -99,6 +112,18 @@ public class Advent2024_12_1 implements CommandLineRunner {
         return new Perimeter(c, region);
     }
     
+    private List<Corner> findCorners(char[][] map, List<Region> regions) {
+        return regions.stream().map(r -> {
+            val c = findCorner(map, r);
+            // log.info("Region {} corner count {}", r.plant, c.count);
+            return c;
+        }).toList();
+    }
+
+    private Corner findCorner(char[][] map, Region region) {
+        val c = region.points.stream().mapToInt(p -> p.cornerCount(map)).sum();
+        return new Corner(c, region);
+    }    
 
     private record Point(int x, int y) {
         boolean isOnMap(char[][] map) {
@@ -111,7 +136,35 @@ public class Advent2024_12_1 implements CommandLineRunner {
                 return (!p.isOnMap(map) || map[p.y][p.x] != map[this.y][this.x]);
             }).count();
         }
+
+        int cornerCount(char[][] map) {
+            return cornerCorner(map, this);
+        }
+
+        char getChar(char[][] map) {
+            return map[y][x];
+        }
+
+        boolean matches(char[][] map, char plant) {
+            return (isOnMap(map) && map[y][x] == plant);
+        }
     }
+
+    static int cornerCorner(char[][] map, Point p) {
+        return MOVE_NEXT_C.stream().mapToInt(mn -> {
+            val pts = mn.apply(p);
+
+            if (pts[0].matches(map, p.getChar(map)) && !pts[1].matches(map, p.getChar(map)) && pts[2].matches(map, p.getChar(map))) {
+                return 1;
+            }
+            else if (!pts[0].matches(map, p.getChar(map)) && !pts[2].matches(map, p.getChar(map))) {
+                return 1;
+            } else {
+                return 0;
+            }
+        }).sum();
+    }
+
 
     private char[][] copyMap(char[][] map) {
         val newMap = new char[map.length][];
